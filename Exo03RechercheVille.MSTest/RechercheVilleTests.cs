ee694f219afce6d0174b7ee1c2dd62bd79ee0b12
gmasquelier <gmasquelier@gmail.com>
2024-01-25 14:26:04 +0100

    RED-When_SearchTextLength_LessThan2_Then_ThrowNotFoundException

--- Exo03RechercheVille.MSTest/RechercheVilleTests.cs
+++ Exo03RechercheVille.MSTest/RechercheVilleTests.cs
@@ -3,9 +3,22 @@ namespace Exo03RechercheVille.MSTest
     [TestClass]
     public class RechercheVilleTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        private RechercheVille _rechercheVille;
+
+        public RechercheVilleTests()
         {
+            Setup();
+        }
+
+        private void Setup()
+        {
+            _rechercheVille = new RechercheVille();
+        }
+
+        [TestMethod]
+        public void When_SearchTextLength_LessThan2_Then_ThrowNotFoundException()
+        {
+            throw new NotImplementedException();
         }
     }
 }
